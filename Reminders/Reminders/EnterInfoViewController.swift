a96ad2e75b0b9d8f858a9e49a87342b3686fc7a9
JeffCui55 <jeffcui55@gmail.com>
2016-02-26 21:14:11 -0500

    added scrollview

--- Reminders/Reminders/EnterInfoViewController.swift
+++ Reminders/Reminders/EnterInfoViewController.swift
@@ -21,9 +21,15 @@ class EnterInfoViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        view.addGestureRecognizer(tap)
     }
 
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
